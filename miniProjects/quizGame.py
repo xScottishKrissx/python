e6a2e97286d75cf94c6221a45b6cb19579e66c43
Chris Dunne <chrisdunne66@gmail.com>
2022-06-27 17:55:01 +0100

    0.3.1 - Quiz Game Tweaks

--- miniProjects/quizGame.py
+++ miniProjects/quizGame.py
@@ -1,40 +1,51 @@
 print("Welcome to my Partick Thistle Quiz :)")
 
-playing = input("Are you ready to play? ").lower()
+playing = input("Are you ready to play? A point for right answers, point off for wrong ones. You ready? ").lower()
 
 if playing != "yes" :
     quit()
 
 print("Okay, let's play :-D ")
-
+print("------------------------")
 
 score = 0
 
 #Q1
 answer = input("Where do Thistle play? ").lower()
-
 if answer == "firhill" :
-    print("Correct :)")
     score += 1
+    print("Correct :) Your score is " + str(score))
+    print("------------------------")
 else :
-    print("Incorrect :(")
+    if score > 0:
+        score -= 1
+    print("Oops :( , i have to take a point off. Your score is " + str(score))
+    print("------------------------")
 
 #Q2
 answer = input("Complete the sentence. Thistle play in red and ... " ).lower()
 if answer == "yellow" :
-    print("Correct Again :)")
     score += 1
+    print("Correct :) Your score is " + str(score))
+    print("------------------------")
 else :
-    print("Incorrect :(")
+    if score > 0:
+        score -= 1
+    print("Oops :( , i have to take a point off. Your score is " + str(score))
+    print("------------------------")
 
 #Q3
 answer = input("Which league do Thistle play in? " ).lower()
 if answer == "championship" :
-    print("Correct Again Again :)")
     score += 1
+    print("Correct :) Your score is " + str(score))
+    print("------------------------")
 else :
-    print("Incorrect :(")
+    if score > 0:
+        score -= 1
+    print("Oops :( , i have to take a point off. Your score is " + str(score))
+    print("------------------------")
 
 #End of Program
-print("Woohoo, You scored " + str(score) + "/3")
-print("That was " + str((score / 4) * 100) + "%")
+print("GAME OVER !!!!!!!!!!!!!! ")
+print("You scored " + str(score) + "/3. That was " + str((score / 3) * 100) + "%")
