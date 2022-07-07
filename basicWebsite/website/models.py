504ace5d878c33d949cd677eb68e9c52dc442b36
Chris Dunne <chrisdunne66@gmail.com>
2022-07-07 15:55:44 +0100

    0.8.7 - Database Models

---
+++ basicWebsite/website/models.py
@@ -0,0 +1,16 @@
+from enum import unique
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    firstName = db.Column(db.String(150))
