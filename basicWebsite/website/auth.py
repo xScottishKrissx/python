5b1ec384af6f1026d7c66f251b307c45281cb944
Chris Dunne <chrisdunne66@gmail.com>
2022-07-07 16:25:08 +0100

    0.8.10 - Creating New User Account

--- basicWebsite/website/auth.py
+++ basicWebsite/website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -16,7 +19,7 @@ def logout():
 def signUp():
     if request.method == "POST":
         email = request.form.get("email")
-        first_name = request.form.get("firstName")
+        first_name = request.form.get("first_name")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
@@ -26,10 +29,17 @@ def signUp():
             flash("First name must be greater than 1 character", category="error")
         elif password1 != password2:
             flash("Password must match", category="error")
-        elif len(password1) < 7:
+        elif len(password1) < 4:
             flash("Password must be greater than 7 characters", category="error")
         else:
             # Add User to Database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+
             flash("Account created", category="Success")
+
+            return redirect(url_for("views.home"))
+
     return render_template('signUp.html')
 
